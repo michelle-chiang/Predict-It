3049d694e5c2d14473188f5e1a7daf2173791e50
Michelle Chiang <michellechiang88@gmail.com>
2018-04-27 00:44:36 -0400

    Tasks -> Today's To-Do List

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -6,7 +6,7 @@ import Notifications from 'views/Notifications/Notifications';
 const appRoutes = [
     { path: "/dashboard", name: "Dashboard", icon: "pe-7s-graph", component: Dashboard },
     { path: "/table", name: "Table Data", icon: "pe-7s-menu", component: TableList },
-    { path: "/tasks", name: "Tasks", icon: "pe-7s-note2", component: Notifications },
+    { path: "/tasks", name: "Today's To-Do List", icon: "pe-7s-note2", component: Notifications },
     { path: "/user", name: "Restaurant Profile", icon: "pe-7s-user", component: UserProfile },
 	{ redirect: true, path:"/", to:"/dashboard", name: "Dashboard" }
 ];
